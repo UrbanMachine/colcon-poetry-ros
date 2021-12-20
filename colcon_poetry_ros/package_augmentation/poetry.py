944342d3dcb0880d28d86866abdd2a48776d35ff
Tyler Compton <xaviosx@gmail.com>
2021-12-19 22:28:05 -0800

    Install deps in the package's isolated location

--- colcon_poetry_ros/package_augmentation/poetry.py
+++ colcon_poetry_ros/package_augmentation/poetry.py
@@ -1,15 +1,14 @@
-import subprocess
 import shutil
-from typing import List, Set
 
 import toml
 from colcon_core.package_augmentation import PackageAugmentationExtensionPoint
 from colcon_core.package_descriptor import PackageDescriptor
 from colcon_core.plugin_system import satisfies_version
 from colcon_core.package_augmentation.python import \
-    create_dependency_descriptor
+    create_dependency_descriptor, logger
 
 from colcon_poetry_ros import config
+from colcon_poetry_ros.package_identification.poetry import ROSPoetryProject
 
 
 class PoetryPackageAugmentation(PackageAugmentationExtensionPoint):
@@ -29,12 +28,8 @@ class PoetryPackageAugmentation(PackageAugmentationExtensionPoint):
             # Some other identifier claimed this package
             return
 
-        lock_file = desc.path / "poetry.lock"
-        if not lock_file.is_file():
-            raise RuntimeError(
-                f"The lock file is missing, expected at '{lock_file}'. Have you run "
-                f"'poetry lock'?"
-            )
+        project = ROSPoetryProject(desc.path, logger)
+        project.check_lock_file_exists()
 
         if not shutil.which("poetry"):
             raise RuntimeError(
@@ -50,8 +45,8 @@ class PoetryPackageAugmentation(PackageAugmentationExtensionPoint):
         else:
             build_deps = set()
 
-        run_deps = _get_dependencies(desc, extras=config.run_depends_extras.get())
-        test_deps = _get_dependencies(desc, extras=config.test_depends_extras.get())
+        run_deps = project.get_dependencies(config.run_depends_extras.get())
+        test_deps = project.get_dependencies(config.test_depends_extras.get())
 
         desc.dependencies["build_depends"] = set(
             create_dependency_descriptor(dep) for dep in build_deps
@@ -62,61 +57,3 @@ class PoetryPackageAugmentation(PackageAugmentationExtensionPoint):
         desc.dependencies["test_depends"] = set(
             create_dependency_descriptor(dep) for dep in test_deps
         )
-
-
-def _get_dependencies(
-    desc: PackageDescriptor,
-    extras: List[str],
-) -> Set[str]:
-    command = ["poetry", "export", "--format", "requirements.txt"]
-
-    for extra in extras:
-        command += ["--extras", extra]
-
-    try:
-        result = subprocess.run(
-            command,
-            cwd=desc.path,
-            stdout=subprocess.PIPE,
-            check=True,
-            encoding="utf-8",
-        )
-    except subprocess.CalledProcessError as ex:
-        raise RuntimeError(
-            f"Failed to export Poetry dependencies in the requirements.txt format: "
-            f"{ex}"
-        )
-
-    return _parse_requirements(result.stdout)
-
-
-def _parse_requirements(input_: str) -> Set[str]:
-    """Parses Python dependencies in the requirements.txt format.
-
-    :param input_: The text in the requirements.txt
-    :return: A list of dependencies in PEP440 format
-    """
-    specifications: List[str] = []
-    dependency = ""
-
-    for line in input_.split("\n"):
-        # Whitespace around definitions is never semantically significant
-        line = line.strip()
-
-        if line.startswith("#"):
-            # Just a comment, ignore it
-            continue
-        elif len(line) == 0:
-            continue
-
-        dependency += line
-        if line.endswith("\\"):
-            # Line continuation! The dependency definition continues
-            dependency += line[:-1]
-        else:
-            # The dependency definition is complete. Remove any environment
-            # markers, leaving only the specification.
-            spec = dependency.split(";")[0]
-            specifications.append(spec)
-
-    return set(specifications)
